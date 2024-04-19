7f5f8e8ac2d9fc6586a3003ce63d01bae6af668c
Azofo <148377511+Azofo@users.noreply.github.com>
2024-04-19 13:09:27 +0200

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,35 @@
+# Primer hola mundo en Python
+print('Hola Mundo!')
+'''
+Esto es 
+un comentario de 
+varias líneas
+'''
+print("me llamo pepito")
+print(3.4)
+print(5)
+new_var = True
+print(new_var)
+print(5 + 8)
+print(7 + 3.4)
+print("32 + 34")
+print("32"+"34")
+print("34", + 34)
+print(int("34")+34)
+print(int(new_var)+34)
+print(type("tr"))
+print(type(print("a ver")))
+print(str(34)+"asda")
+print(34.565123123,end="6")
+print()
+name, surname, age = "Juan", "Pérez", 65
+frase = "Me llamo "+name+" "+surname+" y tengo "+ str(age) +" años"
+print (frase)
+name, email, password = "María", "jimenez@gmail.com", "123124"
+print("Hola, ",name," ¿tu email es ",email, " y tu password ", password, " son correctos?" )
+
+#inputs
+
+name = input("Cual es tu nombre: ")
+print (name)
+
